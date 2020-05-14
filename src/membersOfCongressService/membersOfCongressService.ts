078ee671979b1c3b180b95eea9268e06c3caa380
Michael Smith <michael.christian.smith@gmail.com>
2020-05-13 22:40:44 -0600

    update

--- src/membersOfCongressService/membersOfCongressService.ts
+++ src/membersOfCongressService/membersOfCongressService.ts
@@ -1,7 +1,7 @@
-import { REPRESENTATIVES } from "../houseSelect/HouseSelect";
+const DEFAULT_API_HOST = 'http://localhost:3000/';
 
 let getMembersOfCongress = async (house: string, state: string) => {
-    let membersResponse = await fetch(`https://whoismyrepresentative.com/getall_${house === REPRESENTATIVES ? 'reps' : 'sens'}_bystate.php?state=${state}&output=json`);
+    let membersResponse = await fetch(`${process.env.REACT_APP_API_HOST || DEFAULT_API_HOST}${house}/${state}`);
     return (await membersResponse.json()).results;
 };
 
